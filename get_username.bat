6bfcf563ec2083efde8ce985ea336098a7bcc871
shu-1167 <shu_11671@yahoo.co.jp>
2019-06-06 15:54:21 +0900

    add README and define variable

--- get_username.bat
+++ get_username.bat
@@ -3,31 +3,44 @@ setlocal enabledelayedexpansion
 
 rem MAETELドメインのprofessorsグループに所属しているユーザーの名前を取得する
 rem 同一ディレクトリにあるprofessors_user.txtにあるユーザー名から名前取得
+rem 初期変数変更で対象変更可(実行ユーザー、ドメイン)
 
-for /f "usebackq" %%i in (`icacls %~0 ^| find "2180271"`) do (
+set RUN_USER=2180271
+rem ドメインは大文字のほうがいいかも
+set DOMAIN=MAETEL
+
+
+for /f "usebackq" %%i in (`icacls %~0 ^| find "%RUN_USER%"`) do (
+	rem 実行ファイルの権限チェック
 	set /a isAcs=1
 )
 
 if not defined isAcs (
-	icacls %~0 /grant maetel\2180271:F > NUL
+	rem 権限がなければ許可
+	icacls %~0 /grant %DOMAIN%\%RUN_USER%:F > NUL
 )
 
 
-if not "%USERDOMAIN%" == "MAETEL" (
-	runas /noprofile /user:maetel\2180271 %~0
+if not "%USERDOMAIN%" == "%DOMAIN%" (
+	rem ドメインユーザーでなければドメインユーザーで再実行
+	runas /noprofile /user:%DOMAIN%\%RUN_USER% %~f0
 	exit /b
 )
 
-del %~p0\professors_name.csv
-type nul > %~p0\professors_name.csv
-icacls %~p0\professors_name.csv /grant maetel\2180271:F > NUL
-for /f %%i in (%~p0\professors_user.txt) do (
+rem 過去ファイルの削除、新ファイルの作成
+del %~p0professors_name.csv
+type nul > %~p0professors_name.csv
+icacls %~p0professors_name.csv /grant %DOMAIN%\%RUN_USER%:F > NUL
+
+for /f %%i in (%~p0professors_user.txt) do (
+	rem ユーザー数分ループ
 	set /a count=0
 	echo %%i
 	for /f "usebackq tokens=3,4 skip=2" %%j in (`net user %%i /domain`) do (
 		set /a count=!count!+1
 		if !count! EQU 1 (
-			echo %%i,%%j,%%k>>%~p0\professors_name.csv
+			rem 名前部分のみ抽出
+			echo %%i,%%j,%%k>>%~p0professors_name.csv
 		)
 	)
 )
--- professors_user.bat
+++ professors_user.bat
@@ -1,26 +1,40 @@
 @echo off
 setlocal enabledelayedexpansion
-rem MAETELドメインのprofessorグループのユーザーを抽出する
+rem MAETELドメインのprofessorsグループのユーザーを抽出する
+rem 初期変数変更で対象変更可(実行ユーザー、ドメイン、グループ)
 
-for /f "usebackq" %%i in (`icacls %~0 ^| find "2180271"`) do (
+set RUN_USER=2180271
+rem ドメインは大文字のほうがいいかも
+set DOMAIN=MAETEL
+set GROUP=professors
+
+
+for /f "usebackq" %%i in (`icacls %~0 ^| find "%RUN_USER%"`) do (
+	rem 実行ファイルの権限チェック
 	set /a isAcs=1
 )
 
 if not defined isAcs (
-	icacls %~0 /grant maetel\2180271:F > NUL
+	rem 権限がなければ許可
+	icacls %~0 /grant %DOMAIN%\%RUN_USER%:F > NUL
 )
 
 
-if not "%USERDOMAIN%" == "MAETEL" (
-	runas /noprofile /user:maetel\2180271 %~0
+if not "%USERDOMAIN%" == "%DOMAIN%" (
+	rem ドメインユーザーでなければドメインユーザーで再実行
+	runas /noprofile /user:%DOMAIN%\%RUN_USER% %~f0
 	exit /b
 )
 
-del %~p0\professors_user.txt
-type nul > %~p0\professors_user.txt
-icacls %~p0\professors_user.txt /grant maetel\2180271:F > NUL
-for /f "usebackq skip=8 tokens=1-3 delims= " %%i in (`net group professors /domain`) do (
+rem 過去ファイルの削除、新ファイルの作成
+del %~p0professors_user.txt
+type nul > %~p0professors_user.txt
+icacls %~p0professors_user.txt /grant %DOMAIN%\%RUN_USER%:F > NUL
+
+for /f "usebackq skip=8 tokens=1-3 delims= " %%i in (`net group %GROUP% /domain`) do (
+	rem ユーザーID部分のみ抽出
 	for /l %%a in (1,1,3) do (
+		rem 1行に3ユーザー表示されるので分割
 		if "%%a" == "1" (
 			set user=%%i
 		) else if "%%a" == "2" (
@@ -28,6 +42,6 @@ for /f "usebackq skip=8 tokens=1-3 delims= " %%i in (`net group professors /doma
 		) else (
 			set user=%%k
 		)
-        echo !user!>>%~p0\professors_user.txt
+        echo !user!>>%~p0professors_user.txt
     )
 )
