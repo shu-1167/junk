53c872f4ad1ade48b43be37df27bd75c0fb1602e
shu-1167 <shu_11671@yahoo.co.jp>
2019-06-19 18:03:45 +0900

    Add chcp and fix some bugs

--- professors_user.bat
+++ professors_user.bat
@@ -8,6 +8,12 @@ rem ドメインは大文字のほうがいいかも
 set DOMAIN=MAETEL
 set GROUP=professors
 
+rem 現在のコードページを取得
+for /f "usebackq tokens=2 delims=:" %%i in (`chcp`) do (
+	set def_codepage=%%i
+)
+rem 英語環境へ
+chcp 65001 > NUL
 
 for /f "usebackq" %%i in (`icacls %~0 ^| find "%RUN_USER%"`) do (
 	rem 実行ファイルの権限チェック
@@ -42,6 +48,20 @@ for /f "usebackq skip=8 tokens=1-3 delims= " %%i in (`net group %GROUP% /domain`
 		) else (
 			set user=%%k
 		)
-        echo !user!>>%~p0professors_user.txt
+		rem 空欄、最終行は無視
+		if not "!user!" == "" (
+			if "%%i" == "The" (
+				if "%%j" == "command" (
+					if "%%k" == "completed" (
+						goto :finish
+					)
+				)
+			)
+			echo !user!>>%~p0professors_user.txt
+		)
     )
 )
+
+:finish
+rem 元のコードページへ
+chcp%def_codepage% > NUL
